bb0e7a9b1f002fbcf80521d6028ff1ab2e1f5038
Mohamed Gamal Eldin <muhamed.gamal9876@gmail.com>
2021-07-30 11:35:44 +0200

    style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import Css from './header.module.css'
 const Header = (props) => {
   return (
     <header>
-      <h1>{props.data} / {props.title}</h1>
+      <h1
+        style={{color: 'gray'}}
+        >
+        {props.data} / {props.title}
+      </h1>
+      <h1
+        style={StyleHeader}
+        >
+        Another Way to Use Style
+      </h1>
+      <h1
+        className={Css.header}
+        >
+        Another Way to Use Style
+      </h1>
     </header>
   )
 }
@@ -16,6 +30,13 @@ Header.defaultProps = {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+}
+
+const StyleHeader = {
+  color: 'green',
+  float: 'right',
+  marginTop: 0,
+
 }
 
 export default Header
